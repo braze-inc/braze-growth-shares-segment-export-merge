f39857318a031ebb7b350d72ba432763a482db0a
“zzhaobraze” <Zeyuan.zhao@braze.com>
2020-10-23 12:21:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Braze User Export Segment Merge
+This is a python 3 script which simplifies the process of combining multiple Braze Segment CSV export into a single file. The following file format is supported: `.gz`, `.zip`,`.txt`,`.tab`,`.csv`. When using a `tab` or `csv` delimited files, make sure the `delimiter` setting is set correctly.
+
+## Requirements
+The following are required:
+* [python3](https://www.python.org/) installed
+	* Optional, [venv](https://docs.python.org/3/library/venv.html) installed
+
+## Process Steps
+The following is an outline of the process:
+* Edit the `.env` with any custom `exportfields` for output ie `external_id,email`. If using `combineonly`, then the files will only be combined. **Warning, please make sure all files are in the correct format, and that the header lines up. NO file transformation is done**
+* Edit the `.env` with any `delimiter` and  `outputdelimiter` changes. ie `comma` or `tab`
+* Run the script using `python3 merge_exports.py` to read the export files, extract the specific fields, and combine the files into one file.
+* Results are saves it to the `outpath`
+
+# Setup instructions
+To run the script, [python3](https://www.python.org/) is required to be installed. Optionally, having [venv](https://docs.python.org/3/library/venv.html) installed is recommended.
+
+## Configuration
+The easiest way to setup the configuration is to create a `.env` file in the same path as the script. See [.sample_env](.sample_env) for an example to copy and `rename`.
+
+### Configuration Settings
+|Setting|Description|Example value|
+|----|----|----|
+|exportfields|Fields to be read from the csv files|external_id,attr_1,attr_2|
+|combineonly|Boolean - false, enable to only combine the files, and csv columns will not be parsed. **WARNING: Ensure all columns are align in all files** |false|
+|outputpath|Folder to place the results in. Make sure the folder exist.|done|
+|outputname|Output File prefix name|braze_export|
+|delimiter|File format to read the files, set to `comma` or `tab`| comma|
+|outputdelimiter|File format to write the files, set to `comma` or `tab`| comma|
+
+# Running the Script
+To run the script use `python3 merge_exports.py`. This will read each export zip file from the `exportdir`, process the files based on the delimiter type, and place the resulting file into the `outputpath` folder.
+
+## Install dependency
+Use `pip3 install -r requirements.txt` to installed the dependencies. See [Using Virtual environment](#using-virtual-environment) below to avoid dependency issues.
+
+## Using Virtual environment
+To avoid dependency issues, it's recommend to use venv.
+* `python3 -m venv ./.export` - creates the virtual environment
+* `source .export/bin/activate` - activate the virtual environment
+* `pip3 install -r requirements.txt` - install dependency if not already installed
+* `python3 merge_exports.py` - runs the script for processing the export files
---
+++ merge_exports.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import collections
+import os
+
+from datetime import datetime, timedelta
+import re
+import gzip
+import json
+import requests
+import csv
+import urllib3
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+import io
+from io import TextIOWrapper
+from zipfile import ZipFile
+import logging
+import boto3
+import gc
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Method to process s3 bucket, and save to cloud storage
+# All files within the data directory will be process then moved to a done folder.
+def mergeExports():
+    exportsdir = './' + os.environ['exportdir'].rstrip('/').lstrip('/').strip() + '/'
+    brazefields = [sf.strip() for sf in os.environ['exportfields'].split(',')]
+
+    dateprefix = datetime.utcnow().strftime('%Y-%m-%d_%H%M%S')
+    fileprefix ="./"
+    if ('outputpath' in os.environ) and  (os.environ['outputpath'].strip()):
+        fileprefix += os.environ['outputpath'].strip().rstrip('/').lstrip('/') + '/'
+
+
+
+    extract = True
+    extension = 'csv'
+
+    if ('extractfiles' in os.environ) and (os.environ['extractfiles'].strip().lower() in ['false', '0', 'f', 'n', 'no']):
+        extract = False
+    combineonly = False
+    if ('combineonly' in os.environ) and (os.environ['combineonly'].strip().lower() in ['true', '1', 't', 'y', 'yes']):
+        combineonly = True
+    delimiter = ','
+    if ('delimiter' in os.environ) and (os.environ['delimiter'].strip().lower() in ['tab']):
+        delimiter = '\t'
+    outdelimiter = ','
+    if ('outputdelimiter' in os.environ) and (os.environ['outputdelimiter'].strip().lower() in ['tab']):
+        outdelimiter = '\t'
+        extension = 'txt'
+
+    outfile = open("{}{}_{}_results.{}".format(fileprefix,os.environ['outputname'].strip(),dateprefix,extension), "w")
+    if not combineonly:
+        csvwriter = csv.writer(outfile, delimiter=outdelimiter)
+        outfile.write(outdelimiter.join(brazefields) + "\n")
+
+    exportslist = os.scandir(exportsdir)
+    for exportfile in exportslist:
+        if (exportfile.is_file()):
+            if (exportfile.name.endswith(('.zip', '.gz'))):
+                with ZipFile(exportfile, 'r') as zip:
+                    for csvs in zip.infolist():
+                        print("Processing file {} in {}".format(csvs.filename,exportfile.name))
+                        csvfile = zip.open(csvs, 'r')
+
+                        if combineonly:
+                            # for rows in csvfile:
+                            # csvfile = zip.read(csvs, 'r')
+                            csvdata = TextIOWrapper(csvfile, 'utf-8')
+                            for rows in csvdata:
+                                outfile.write(rows)
+                        else:
+                            csvdata = csv.DictReader(TextIOWrapper(csvfile, 'utf-8'), delimiter=delimiter)
+                            for records in csvdata:
+                                row = []
+                                for fields in brazefields:
+                                    if fields in records:
+                                        row.append(records[fields])
+                                    else:
+                                        row.append(None)
+                                        # print(records[fields], end=',')
+                                csvwriter.writerow(row)
+            elif exportfile.name.endswith(('.txt','.csv','.tab')):
+                print("Processing file {}".format(exportfile.name))
+                csvfile = open(exportfile, 'r')
+
+                if combineonly:
+                    # csvfile = read(exportfile, 'r')
+                    for rows in csvfile:
+                        outfile.write(rows)
+                else:
+                    csvdata = csv.DictReader(csvfile, delimiter=delimiter)
+                    for records in csvdata:
+                        row = []
+                        for fields in brazefields:
+                            if fields in records:
+                                row.append(records[fields])
+                            else:
+                                row.append(None)
+                                # print(records[fields], end=',')
+                        csvwriter.writerow(row)
+    print("Results saved in {}".format(outfile.name))
+
+if __name__ == '__main__':
+    mergeExports()
